5412abd2e66f697faf097e3c51f7c3fbf7d07e4a
Crypto <ispa198@gmail.com>
2021-01-12 16:57:26 +0530

    2st commit

--- app/src/main/java/com/example/forgot_password/MainActivity.java
+++ app/src/main/java/com/example/forgot_password/MainActivity.java
@@ -1,47 +1,28 @@
 package com.example.forgot_password;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Fragment fragment = null;
+    Button  stbbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ChipNavigationBar chipNavigationBar = findViewById(R.id.bottom);
+        stbbtn = findViewById(R.id.submit_button);
 
-        chipNavigationBar.setItemSelected(R.id.home, true);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
+        stbbtn.setOnClickListener(new View.OnClickListener() {
 
-        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
             @Override
-            public void onItemSelected(int i) {
-                switch (i) {
-                    case R.id.fpbm_id:
-                        fragment = new HomeFragment();
-                        break;
-                    case R.id.otpbm_id:
-                        fragment = new OTPFragment();
-                        break;
-                    case R.id.spinnerbm_id:
-                        fragment = new SpinnerFragment();
-                        break;
-
-                }
-
-                if (fragment != null) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-                }
+            public void onClick(View v) {
+                Intent in = new Intent(getBaseContext(), searchable_spinner.class);
+                startActivity(in);
             }
         });
     }
--- app/src/main/java/com/example/forgot_password/SpinnerFragment.java
+++ app/src/main/java/com/example/forgot_password/SpinnerFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 
@@ -45,12 +47,19 @@ public class SpinnerFragment extends Fragment {
         return fragment;
     }
 
+//    Spinner spinner;
+//    TextView textView;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
     }
 
@@ -58,6 +67,16 @@ public class SpinnerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spinner, container, false);
+        View v = inflater.inflate(R.layout.fragment_spinner, container, false);
+
+        String [] values =
+                {"Time at Residence","Under 6 months","6-12 months","1-2 years","2-4 years","4-8 years","8-15 years","Over 15 years",};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        return v;
+        //return inflater.inflate(R.layout.fragment_spinner, container, false);
     }
 }
